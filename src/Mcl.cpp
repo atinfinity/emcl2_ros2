2a3b3063e41e53d8c4aeb13a8e950b7cd82d22b1
atinfinity <dandelion1124@gmail.com>
2026-06-09 21:14:40 +0900

    fixed to use laser_min_range and laser_max_range

--- src/Mcl.cpp
+++ src/Mcl.cpp
@@ -229,8 +229,6 @@ void Mcl::setScan(const sensor_msgs::msg::LaserScan::ConstSharedPtr msg)
 	scan_.angle_min_ = msg->angle_min;
 	scan_.angle_max_ = msg->angle_max;
 	scan_.angle_increment_ = msg->angle_increment;
-	scan_.range_min_ = msg->range_min;
-	scan_.range_max_ = msg->range_max;
 }
 
 double Mcl::normalizeBelief(void)
